518ee2e32aac1e53d064ffd292f7d8cf7ff6f5d9
Shipra-AGRA <shipraagrawal230@gmail.com>
2025-03-25 13:39:43 +0530

    add restaurant filter

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,22 @@
 import RestaurantCard from "./RestaurantCard"
 import resObj from "../utils/mockData"
+import React from "react"
+import { useState } from "react"
 
 const Body = () => {
+    const [ListOfRestaurant,setListOfRestaurant]=useState(resObj)
+
     return (
         <div className='body'>
-            <div className='search'>search</div>
+            <div className='filter'>
+                <button className="filter-btn" onClick={()=>{
+                    const filteredList=ListOfRestaurant.filter((res)=>res.info.avgRating>4.3)
+                    setListOfRestaurant(filteredList)
+                }}>Top Rated Restaurant</button>
+            </div>
             <div className='res-container'>
                 {
-                    resObj.map((restaurant)=>(
+                    ListOfRestaurant.map((restaurant)=>(
                         <RestaurantCard resData={restaurant} key={restaurant.info.id}/>
                     ))
                 }
